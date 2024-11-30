81b29561f011f3f3399ccaaaaf373a071e0190af
Caguicla, Ian Kenneth T. <153847419+Caguicla@users.noreply.github.com>
2024-11-30 23:01:06 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Function to show an alert with the skills list
+function showSkills() {
+    alert("Skills: HTML, CSS, JavaScript, React, Node.js, Git & GitHub, Responsive Design, Problem-solving");
+}
